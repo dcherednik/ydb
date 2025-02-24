d48f0ccf49e3670fc37f3c8679aafd70e8c137a5
Daniil Cherednik <dcherednik@ydb.tech>
2025-02-24 12:40:36 +0100

    copy paste grpc code to read MSG_ERRQUEUE

--- ydb/library/actors/interconnect/interconnect_tcp_session.h
+++ ydb/library/actors/interconnect/interconnect_tcp_session.h
@@ -437,6 +437,8 @@ namespace NActors {
             return ReceiveContext->ClockSkew_us;
         }
 
+        void ProcessErrQueue(NInterconnect::TStreamSocket& socket);
+
     private:
         friend class TInterconnectProxyTCP;
 
